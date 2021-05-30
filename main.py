db84a94690294ef4aa42d9277dc0d77193e455c7
hamu1011 <akang.mufti@gmail.com>
2021-05-31 01:45:06 +0200

    new reading format that more adaptable to any changes in xdk for reading at the same time or at different time

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
 import socket
-import traceback
 from XDKMeasurement import DBInsert, XDKMeasurementQueue, DBManager
-from datetime import datetime, timedelta
+from datetime import datetime
 
-UDP_IP = "192.168.0.74"
+UDP_IP = "192.168.178.24"
 UDP_PORT = 5005
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
@@ -28,50 +27,23 @@ if __name__ == '__main__':
     workTime = datetime.now()
 
     while True:
-        start = datetime.now()
-
         rxData, addr = sock.recvfrom(1024)  # buffer size is 1024 bytes
         dataset = str(rxData, "utf-8")
         dataRaw = dataset.split('\t')
-        #print(dataRaw)
 
-        stop = datetime.now()
-        dT = stop - start
-        if dT > timedelta(seconds=1):
-            counter = counter + 1
-            resetTime = start
-            ActiveDuration = resetTime - workTime
-            print("Active time\t:", ActiveDuration)
-            workTime = stop
-            # after reset
-            print("Reset at\t:", start.time())
-            print("Start at\t:", stop.time())
-            ResetDuration = stop - start
-            print("Reset time\t:", ResetDuration)
-
-        if len(dataRaw) > 1:
-            time = dataRaw[0]
-            sensorsData = dataRaw[1:]
-        else:
-            sensorsData = dataRaw
+        # get time sntp or rtc - time in xdk --> if the value default then use python time
+        measurementTime = datetime.strptime(dataRaw[0], "%Y-%m-%dT%H:%M:%SZ") \
+            if "2000-01-01T00:00:00Z" != dataRaw[0] else datetime.now()
+        sensorsData = dataRaw[1:]
 
         for data in sensorsData:
             incomingData = data.split(';')
-            # print(incomingData)
-
             try:
-                sensor = incomingData[0]
-                if len(dataRaw) > 1:
-                    measurementTime = datetime.strptime(time, "%Y-%m-%dT%H:%M:%SZ")
-                    measurementData = incomingData[1:]
-                else:
-                    measurementTime = datetime.strptime(incomingData[1], "%Y-%m-%dT%H:%M:%SZ")
-                    measurementData = incomingData[2:]
+                measurementName = incomingData[0]
+                measurementData = incomingData[1:]
             except ValueError:
-                # print("ERROR: INVALID INCOMING BYTES")
-                # traceback.print_exc()
                 continue
 
             for q in Queues:
-                if sensor == q.Name:
+                if measurementName == q.Name:
                     q.input(measurementTime, measurementData)
